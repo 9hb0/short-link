5612300e1a16271b0fcf72e6064cc788c71d4fd7
Axiaoxin <1924256487@qq.com>
2024-03-19 23:31:42 +0800

    数据库连接

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import com.nageoffer.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
  * 用户管理控制层
  */
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
     /**
      *根据用户名查询用户信息
      */
