6d246c092daff95e365020cf7d85bebdc4348bfe
Axiaoxin <1924256487@qq.com>
2024-06-09 15:44:01 +0800

    修改短链接分组名称

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
@@ -2,6 +2,7 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
+import com.nageoffer.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 
 import java.util.List;
@@ -15,4 +16,6 @@ public interface GroupService extends IService<GroupDO> {
     public void saveGroup(String  groupName);
 //    public void saveGroup(String username, String  groupName);
     List<ShortLinkGroupRespDTO> listGroup();
+
+    void updateGroup(ShortLinkGroupUpdateReqDTO requestParam);
 }
